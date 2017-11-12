ce04ea44a69ca6ebd385c232ce4658b7166aa869
hollownewman <pingfan5922@gmail.com>
2017-11-12 22:34:20 +0800

    blog

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  //var supplies=['1','2','3'];
   res.render('index', { title: 'Express' });
+
+});
+
+router.get('/new', function(req, res, next) {
+  //res.render('index', { title: 'new' });
+  res.send('hello wrold!');
+});
+
+router.get('/reg', function(req, res) {
+    //res.render('index', { title: 'Express' });
+    res.render('reg',{title:'注册'});
+});
+router.get('/login', function(req, res) {
+  //res.render('index', { title: 'Express' });
+  res.render('login',{title:'登录'});
 });
 
 module.exports = router;
